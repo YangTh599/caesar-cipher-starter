ce5f268c6f18a6c608af0b262b75d74c83719c3e
YangTh599 <YangTh599@northwested.org>
2024-10-04 10:28:59 -0400

    took away capitalized alphabet

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 alphabet = 'abcdfeghijklmnopqrstuvwxyz'
 new_message = ''
 
-user_message = input('Enter your secret message:\n')
+user_message = input('Enter your secret message:\n').lower()
 key = int(input('Enter a key: \n'))
 
 for character in user_message:
@@ -14,14 +14,7 @@ for character in user_message:
         new_position = (position + key) % 26
         new_character = alphabet[new_position]
 
-        new_message += new_character
-    elif character in alphabet.upper():
-        upper_alphabet = alphabet.upper()
-        position = upper_alphabet.find(character)
-        new_position = (position + key) % 26
-        new_character = upper_alphabet[new_position]
-
-        new_message += new_character
+        new_message += new_character   
     else:
         new_message += character
 
