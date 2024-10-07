895789cb4bb330cd777772c4f93bb9e84752a4d0
YangTh599 <YangTh599@northwested.org>
2024-10-07 10:35:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,48 @@
 # 02 OCT 2024
 # Caesar Cipher
 
-alphabet = 'abcdfeghijklmnopqrstuvwxyz'
-new_message = ''
+
+def encrypt_message(message,key):
+    alphabet = 'abcdfeghijklmnopqrstuvwxyz'
+    new_message = ''
+
+    for character in message:
+        if character in alphabet:
+            position = alphabet.find(character)
+            new_position = (position + key) % 26
+            new_character = alphabet[new_position]
+
+            new_message += new_character   
+        else:
+            new_message += character
+
+    print('Your encrypted message is \n'+ new_message)
+    return new_message
+
+def decrypted_message(message,key):
+    alphabet = 'abcdfeghijklmnopqrstuvwxyz'
+    new_message = ''
+
+    for character in message:
+        if character in alphabet:
+            position = alphabet.find(character)
+            new_position = (position - key) % 26
+            new_character = alphabet[new_position]
+
+            new_message += new_character   
+        else:
+            new_message += character
+
+    print('Your decrypted message is \n'+ new_message)
+    return new_message
 
 user_message = input('Enter your secret message:\n').lower()
 key = int(input('Enter a key: \n'))
 
-for character in user_message:
-    if character in alphabet:
-        position = alphabet.find(character)
-        new_position = (position + key) % 26
-        new_character = alphabet[new_position]
+secret = encrypt_message(user_message, key)
 
-        new_message += new_character   
-    else:
-        new_message += character
+not_secret = decrypted_message(secret,key)
 
-print('Your new message is \n'+ new_message)
+print()
+print(secret)
+print(not_secret)
